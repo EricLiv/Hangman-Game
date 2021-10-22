e935d5bb404a10178f617143c09c968f237542a7
EricLiv <ELivingston002@my.wilmu.edu>
2021-10-22 14:11:40 -0400

    first commit

---
+++ hangman.py
@@ -0,0 +1,56 @@
+import random
+
+# Define the main function for the hangman game.
+
+
+def hangman():
+    # List the words that will be randomly selected for game.
+    word_list = ["python", "Java", "computer", "hacker", "painter", "coding", "Cybersecurity", "Computer Science", "Web Development",
+                 "Data Science", "Software Enginerring", "Linux", "WireShark", "encryption", "GitHub", "Django", "Reddit", "technology", "Geeking"]
+    # Randomize the word list - words selected randomly.
+    random_number = random.randint(0, 18)
+    word = word_list[random_number]
+    wrong_guesses = 0
+    # Stages to build the hanging man for each wrong guess.
+    stages = ["",
+              "________      ",
+              "|             ",
+              "|      |      ",
+              "|      0      ",
+              "|     /|\     ",
+              "|     / \     ",
+              "|             ",
+              ]
+    # Letters that make up the word when a correct letter is guessed.
+    remaining_letters = list(word)
+    # Layout for each letter in a word.
+    letter_board = ["__"] * len(word)
+    win = False
+    print('Welcome to Hangman')
+    # -1 to accomodate 0-index
+    while wrong_guesses < len(stages) - 1:
+        print('\n')
+    # Input for player interaction
+        guess = input("Guess a letter")
+        if guess in remaining_letters:
+            character_index = remaining_letters.index(guess)
+            letter_board[character_index] = guess
+            remaining_letters[character_index] = '$'
+        else:
+            wrong_guesses += 1
+        print((' '.join(letter_board)))
+        print('\n'.join(stages[0: wrong_guesses + 1]))
+    # Player wins game if
+        if '__' not in letter_board:
+            print('\n' 'You win! The word was:')
+            print(' '.join(letter_board))
+            win = True
+    # End loop
+            break
+    # Player lose game if
+    if not win:
+        print('\n'.join(stages[0: wrong_guesses]))
+        print('You lose! The word was {}'.format(word))
+
+
+hangman()
