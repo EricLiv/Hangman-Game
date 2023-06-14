e39ea6582fd8ccb558584492102aff05b308eab3
Eric Livingston <elivingston002@my.wilmu.edu>
2023-06-14 12:36:32 -0400

    update

--- hangman.py
+++ hangman.py
@@ -5,14 +5,25 @@ import random
 
 def hangman():
     # List the words that will be randomly selected for game.
-    word_list = ["python", "Java", "computer", "hacker", "painter", "coding", "Cybersecurity", "Computer Science", "Web Development",
-                 "Data Science", "Software Enginerring", "Linux", "WireShark", "encryption", "GitHub", "Django", "Reddit", "technology", "Geeking", "JavaScript", "C Languages", "Kotlin", "scripting", "Git", "Bash", "firewall", "router", "systems admin", "cloud", "security", "Hash Crack", "pass the hash", "social engineering", "red team", "blue team", "ethical hacking", "Nmap", "Kali", "Ubuntu", "Unix", "Windows", "MacOS", "Scrum", "Agile", "AWS", "Bootstrap", "GUI", "security operations center", "networking", "Comptia", "binary code", "sandbox", "demilitarization zone", "backdoor", "virus", "trojan horse", "worm", "OSINT", "Redux", "React", "VMWare", "Oracle", "VirtualBox", "Hack the Box", "Try Hack Me", "Exploit", "msConsole", "Metasploit", "Burp Suite", "full stack", "Hyper Text Markup Language", "Cascading Style Sheets"]
+    word_list = ["python", "Java", "computer", "hacker", "painter", "coding",       "Cybersecurity", "Computer Science", "Web Development",
+                 "Data Science", "Software Enginerring", "Linux", "WireShark", "encryption", "GitHub", "Django", "Reddit", "technology",
+                 "Geeking", "JavaScript", "C Languages", "Kotlin", "scripting", "Git", "Bash", "firewall", "router", "systems admin", "cloud",
+                 "security", "Hash Crack", "pass the hash", "social engineering", "red team", "blue team", "ethical hacking", "Nmap", "Kali", "Ubuntu",
+                 "Unix", "Windows", "MacOS", "Scrum", "Agile", "AWS", "Bootstrap", "GUI", "security operations center", "networking", "Comptia", "binary code",
+                 "sandbox", "demilitarization zone", "backdoor", "virus", "trojan horse", "worm", "OSINT", "Redux", "React", "VMWare", "Oracle", "VirtualBox",
+                 "Hack the Box", "Try Hack Me", "Exploit", "msConsole", "Metasploit", "Burp Suite", "full stack", "Hyper Text Markup Language", "Cascading Style Sheets",
+                "ServiceNow", "Server Side Script", "Client Side Script", "Flow Designer", "Certified System Administrator", "Certified Applicaiton Developer", "Implementation Specialist",
+                "Technical Consultant", "portals", "Applications", "Modules", "Form Designer", "Form Layout", "Database", "Citizen Developer", "no code", "low code", 
+                "Record Producer", "Catalog Item", "Knowledge Base", "Widgets", "Tables", "Business Management", "Asset Management", "Oporations Management", "Service Management",
+                "Security Operations", "DevOps", "Technical Architect", "Master Architect", "Business Analyst", "Program Manager", "Product Owner", "RACI", "CRUD", "Stakeholders",
+                "Dev Instance", "Test Instance", "Prod Instance", "Go Live", "Iterations", "User Stories", "Unit Test",]
     # Randomize the word list - words selected randomly.
-    random_number = random.randint(0, 71)
+    random_number = random.randint(0, 114)
     word = word_list[random_number]
     wrong_guesses = 0
     # Stages to build the hanging man for each wrong guess.
     stages = ["",
+              "H A N G M A N ",
               "________      ",
               "|      |      ",
               "|      |      ",
@@ -28,7 +39,7 @@ def hangman():
     # Layout for each letter in a word.
     letter_board = ["__"] * len(word)
     win = False
-    print('Welcome to Hangman')
+    print("Welcome to Eric Livingston's Hangman Game!!")
     # -1 to accomodate 0-index
     while wrong_guesses < len(stages) - 1:
         print('\n')
